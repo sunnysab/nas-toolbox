aedfb8e66a5e3fbfa23bacbf3ec700249b9f15f0
sunnysab <i@sunnysab.cn>
2023-08-23 20:42:31 +0800

    Add comment on granularity.

--- tape/src/device/limit.rs
+++ tape/src/device/limit.rs
@@ -5,6 +5,18 @@ use std::result;
 #[repr(C)]
 #[derive(Debug)]
 pub struct BlockLimit {
+    /// The actual granularity is 2 raised to the power of the value.
+    ///
+    /// In computer science and storage technology, "granularity" refers to the smallest unit of data storage and
+    /// retrieval in a magnetic tape. It determines the level of precision for accessing and transferring data on the tape.
+    ///
+    /// A magnetic tape is typically divided into blocks or records, with each block or record containing a certain amount
+    /// of data. The gaps between these blocks or records are known as gaps. Granularity refers to the size or length of
+    /// each block or record, i.e., the amount of data contained within each block or record.
+    ///
+    /// Smaller granularity means smaller-sized blocks or records, while larger granularity means larger-sized blocks or
+    /// records. The choice of granularity affects the storage capacity of the tape, data transfer speed, and the efficiency
+    /// of data access. (by ChatGPT)
     granularity: u32,
     min_block_length: u32,
     max_block_length: u32,
