d661b5b38e6524c45c49e810dd0d6c9cfc6f2a49
sunnysab <i@sunnysab.cn>
2023-08-23 21:36:45 +0800

    Add tape operations.

--- tape/src/device.rs
+++ tape/src/device.rs
@@ -1,6 +1,7 @@
 mod eot;
 mod limit;
 mod locate;
+mod operate;
 mod status;
 mod status_ex;
 
@@ -9,51 +10,6 @@ pub use limit::BlockLimit;
 pub use locate::{Location, LocationBuilder};
 pub use status::{Density, DriverState, TapeStatus};
 use std::os::fd::RawFd;
-
-enum MtOperation {
-    /// Write an end-of-file record
-    MtWEOF = 0,
-    /// Forward space file
-    MtFSF = 1,
-    /// Backward space file
-    MtBSF = 2,
-    /// Forward space record
-    MtFSR = 3,
-    /// Backward space record
-    MtBSR = 4,
-    /// Rewind
-    MtREW = 5,
-    /// Rewind and put the drive offline
-    MtOFFL = 6,
-    /// No operation, sets status only
-    MtNOP = 7,
-    /// Enable controller cach
-    MtCACHE = 8,
-    /// Disable controller cache
-    MtNOCACHE = 9,
-    /// Set block size for device
-    MtSETBSIZ = 10,
-    /// Set density values for device
-    MtSETDNSTY = 11,
-    /// Erase to EOM
-    MtERASE = 12,
-    /// Space to EOM
-    MtEOD = 13,
-    /// Select compression mode 0=off, 1=def
-    MtCOMP = 14,
-    /// Re-tension tape
-    MtRETENS = 15,
-    /// Write setmark(s)
-    MtWSS = 16,
-    /// Forward space setmark
-    MtFSS = 17,
-    /// Backward space setmark
-    MtBSS = 18,
-    /// Load tape in drive
-    MtLOAD = 19,
-    /// Write an end-of-file record without waiting
-    MtWEOFI = 20,
-}
 
 pub struct TapeDevice {
     fd: RawFd,
---
+++ tape/src/device/operate.rs
@@ -0,0 +1,147 @@
+use super::TapeDevice;
+use anyhow::Result;
+
+#[derive(Debug)]
+pub enum Operation {
+    /// Write an end-of-file record
+    WriteEOF = 0,
+    /// Forward space file
+    ForwardSpaceFile = 1,
+    /// Backward space file
+    BackwardSpaceFile = 2,
+    /// Forward space record
+    ForwardSpaceRecord = 3,
+    /// Backward space record
+    BackwardSpaceRecord = 4,
+    /// Rewind
+    Rewind = 5,
+    /// Rewind and put the drive offline
+    Offline = 6,
+    /// No operation, sets status only
+    NOP = 7,
+    /// Enable controller cache
+    EnableCache = 8,
+    /// Disable controller cache
+    DisableCache = 9,
+    /// Set block size for device
+    SetBlockSize = 10,
+    /// Set density values for device
+    SetDensity = 11,
+    /// Erase to EOM
+    EraseToEnd = 12,
+    /// Space to EOM
+    JumpToEnd = 13,
+    /// Select compression mode 0=off, 1=def
+    SetCompression = 14,
+    /// Re-tension tape
+    Retension = 15,
+    /// Write setmark(s)
+    WriteSetmark = 16,
+    /// Forward space setmark
+    ForwardSpaceSetmark = 17,
+    /// Backward space setmark
+    BackwardSpaceSetmark = 18,
+    /// Load tape in drive
+    Load = 19,
+    /// Write an end-of-file record without waiting
+    WriteEofImmediately = 20,
+}
+
+pub struct MtOp {
+    /// Operations defined above
+    op: u16,
+    /// How many of them.
+    /// If you don't understand, see `man mt`
+    count: i32,
+}
+
+mod ioctl_func {
+    use super::MtOp;
+
+    nix::ioctl_write_ptr!(tape_op, b'm', 1u8, MtOp);
+}
+
+impl TapeDevice {
+    fn do_tape_op(&self, op: Operation, count: u32) -> Result<i32> {
+        let ret = unsafe {
+            let mut mt_op: MtOp = std::mem::zeroed();
+            mt_op.op = op as u16;
+            mt_op.count = count as i32;
+            ioctl_func::tape_op(self.fd, &mut mt_op)?
+        };
+
+        Ok(ret as i32)
+    }
+
+    pub fn write_eof(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::EraseToEnd, count).map(|_| ())
+    }
+
+    pub fn write_eof_immediately(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::WriteEofImmediately, count).map(|_| ())
+    }
+
+    pub fn write_setmark(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::WriteSetmark, count).map(|_| ())
+    }
+
+    pub fn forward_space_file(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::ForwardSpaceFile, count).map(|_| ())
+    }
+
+    pub fn backward_space_file(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::BackwardSpaceFile, count).map(|_| ())
+    }
+
+    pub fn forward_space_record(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::ForwardSpaceRecord, count).map(|_| ())
+    }
+
+    pub fn backward_space_record(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::BackwardSpaceRecord, count).map(|_| ())
+    }
+
+    pub fn forward_space_setmark(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::ForwardSpaceSetmark, count).map(|_| ())
+    }
+
+    pub fn backward_space_setmark(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::BackwardSpaceSetmark, count).map(|_| ())
+    }
+
+    pub fn rewind(&self) -> Result<()> {
+        self.do_tape_op(Operation::Rewind, 0).map(|_| ())
+    }
+
+    pub fn rewind_and_offline(&self) -> Result<()> {
+        self.do_tape_op(Operation::Offline, 0).map(|_| ())
+    }
+
+    pub fn load(&self) -> Result<()> {
+        self.do_tape_op(Operation::Load, 0).map(|_| ())
+    }
+
+    pub fn set_block_size(&self, size: u32) -> Result<()> {
+        self.do_tape_op(Operation::SetBlockSize, size).map(|_| ())
+    }
+
+    pub fn set_density(&self, code: u32) -> Result<()> {
+        self.do_tape_op(Operation::SetDensity, code).map(|_| ())
+    }
+
+    pub fn set_compression(&self, enable: bool) -> Result<()> {
+        self.do_tape_op(Operation::SetCompression, enable as u32).map(|_| ())
+    }
+
+    pub fn erase(&self, count: u32) -> Result<()> {
+        self.do_tape_op(Operation::EraseToEnd, count).map(|_| ())
+    }
+
+    pub fn jump_to_eom(&self) -> Result<()> {
+        self.do_tape_op(Operation::JumpToEnd, 0).map(|_| ())
+    }
+
+    pub fn retension(&self) -> Result<()> {
+        self.do_tape_op(Operation::Retension, 0).map(|_| ())
+    }
+}
